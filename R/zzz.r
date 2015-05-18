f6d6ac126ae5915bf976eda7983c642241d86c02
David Nguyen <david.nguyen@avant.com>
2015-05-18 09:22:51 -0700

    added rerun and run_last

--- R/zzz.r
+++ R/zzz.r
@@ -6,9 +6,16 @@
 
   if (exists('run', envir = .GlobalEnv, inherits = FALSE))
     rm('run', envir = .GlobalEnv)
-
   makeActiveBinding('run', function() run_model, .GlobalEnv)
 
+  if (exists('run_next', envir = .GlobalEnv, inherits = FALSE))
+    rm('run_next', envir = .GlobalEnv)  
+  makeActiveBinding("run_next", function() run(, active_runner()$next_stage()), .GlobalEnv)
+  
+  if (exists('rerun', envir = .GlobalEnv, inherits = FALSE))
+    rm('rerun', envir = .GlobalEnv)  
+  makeActiveBinding("rerun", function() run(, active_runner()$current_stage()), .GlobalEnv)
+  
   addTaskCallback(auto_run)
 }
 
